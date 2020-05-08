b2e053a76c7fb8c6c4b867648aa8667b912cd9a4
Ryan Yang <ryan.y.yang@outlook.com>
2020-05-08 00:01:53 -0500

    asdfasdf

--- scratch0.py
+++ scratch0.py
@@ -1,17 +1,17 @@
 import run_raven_new
 import strategy
 
-run_raven_new.run_raven(strategy.confident)
-
-run_raven_new.run_raven(strategy.neutral)
-
-run_raven_new.run_raven(strategy.prudent)
-
-# run_raven_new.run_raven(strategy.confident, prediction_score_name = "optn_score")
+# run_raven_new.run_raven(strategy.confident)
 #
-# run_raven_new.run_raven(strategy.neutral,  prediction_score_name = "optn_score")
+# run_raven_new.run_raven(strategy.neutral)
 #
-# run_raven_new.run_raven(strategy.prudent,  prediction_score_name = "optn_score")
+# run_raven_new.run_raven(strategy.prudent)
+
+run_raven_new.run_raven(strategy.confident, prediction_score_name = "optn_score")
+
+run_raven_new.run_raven(strategy.neutral,  prediction_score_name = "optn_score")
+
+run_raven_new.run_raven(strategy.prudent,  prediction_score_name = "optn_score")
 
 
 
